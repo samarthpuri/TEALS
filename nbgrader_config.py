5cd3c41a0724629b7a3262ccc23aa43ddc966740
sampuri <samarthpuri@email.arizona.edu>
2019-02-20 14:23:36 -0700

    merge

--- nbgrader_config.py
+++
@@ -1,6 +0,0 @@
-%%file nbgrader_config.py
-
-c = get_config()
-
-c.Exchange.course_id = "introduction_to_computer_science"
-c.Exchange.root = "/Users/simon/Google\ Drive/TEALS/Semester\ 2/Jupyter\ Notebooks/introduction-to-computer-science-semester2"
